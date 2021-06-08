5456e70273651c9234024da8a7f58deb747c7341
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-06-08 14:22:10 -0400

    added props and mapping posts.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Post from './Post';
+import VinnyCarp from './images/VinnyCarp.jpg';
+import JoeCarp from './images/JoeCarp.JPG'
+import carpface from './images/carpface.JPG'
 
 function App() {
+  const [ posts, setPosts ] = useState([
+    {
+      username: "Vinny",
+      caption: "What a lunker!",
+      imageURL: VinnyCarp,
+    },
+    {
+      username: "Joe",
+      caption: "Woo!",
+      imageURL: JoeCarp,
+    },
+    {
+      username: "Alissa",
+      caption: "Winner winner fish dinner!",
+      imageURL: carpface,
+    }
+  ]);
   return (
     <div className="app">
 
@@ -12,10 +32,12 @@ function App() {
           alt="Logo Will Go Here." />
       </div>
       <h1>Welcome to FishID.com!</h1>
-      <Post />
-      <Post/>
-      <Post/>
-      <Post/>
+
+      {
+        posts.map(post => (
+          <Post username = {post.username} caption={post.caption} imageURL={post.imageURL}/>
+        ))
+      }
 
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './Post.css';
-import VinnyCarp from './images/VinnyCarp.jpg';
+
 import Avatar from '@material-ui/core/Avatar';
 
 
-function Post(props) {
+function Post({username, caption, imageURL}) {
+    // const {username, caption, imageURL} = props
     return (
         <div className="post">
             <div className="post_header">
                 <Avatar 
                 className="post_avatar"
-                alt="Vinny"
+                alt={username}
                 src="/static/images/avatar1.jpg"/>
-                <h3>Username</h3>
+                <h3>{username}</h3>
             </div>
-            <img className="post_image" src={VinnyCarp} alt="oops... Now where did that go?"></img>
-            <h4 className="post_text"><strong>Vinny:</strong> What a Lunker!</h4>
+            <img className="post_image" src={imageURL} alt={caption ? caption : "oops... Now where did that go?"}></img>
+            <h4 className="post_text"><strong>{username}:</strong> {caption}</h4>
         </div>
     )
 }
-
 export default Post
