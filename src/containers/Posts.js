5da08d5981056cf1717677617c6a4b030f8436bd
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-08-02 12:18:18 -0400

    refactoring.

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import Post from '../components/Post'
 
 function Posts(posts, user) {
-    const allPosts = posts.map(({id, post}) => ( 
-        <Post key={id} postId={id} user={user} username = {post.username} caption={post.caption} imageUrl={post.imageUrl}/> 
-        )
-      )
+
     return (
-        <> {allPosts} </>
+        <> { posts.map(({id, post}) => ( 
+            <Post key={id} postId={id} user={user} username = {post.username} caption={post.caption} imageUrl={post.imageUrl}/> 
+            )
+          )} </>
     )
 }
 
